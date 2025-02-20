3c25a7bfe40ac3b6040b5b31e083d80c0601de28
grzegorz <grzegorz.szczesniak@firma.interia.pl>
2025-02-20 21:40:28 +0100

    add test and new deploy job

---
+++ src/tests/app.test.jsx
@@ -0,0 +1,11 @@
+import { describe, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe("unit tests", () => {
+    test('show React ⚛️ + Vite ⚡ + Replit 🌀', () => {
+      render(<App />);
+      const header = screen.getByRole('heading');
+      expect(header).toBeDefined();
+    });
+  });
