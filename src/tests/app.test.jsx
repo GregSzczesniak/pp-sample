54add2b5083b07678532e5a33867b101d59284a7
grzegorz <grzegorz.szczesniak@firma.interia.pl>
2025-03-03 21:14:16 +0100

    update 14 files code style

--- src/tests/app.test.jsx
+++ src/tests/app.test.jsx
@@ -1,11 +1,11 @@
-import { describe, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { describe, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
 describe("unit tests", () => {
-    test('show React ⚛️ + Vite ⚡ + Replit 🌀', () => {
-      render(<App />);
-      const header = screen.getByRole('heading');
-      expect(header).toBeDefined();
-    });
+  test("show React ⚛️ + Vite ⚡ + Replit 🌀", () => {
+    render(<App />);
+    const header = screen.getByRole("heading");
+    expect(header).toBeDefined();
   });
+});
